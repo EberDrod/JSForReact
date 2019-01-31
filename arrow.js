a6f0a110710e9128de18fca17198f9b60636927a
eberPeña <eberg.pena@gmail.com>
2019-01-31 05:00:41 -0600

    Un recordatorio a Arrow functions

---
+++ arrow.js
@@ -0,0 +1,27 @@
+//Arrow Fuctions
+/*let viajando = function(destino){
+    return `Viajando ala ciudad de: ${destino}`;
+}
+
+let viaje; 
+viaje = viajando ('Paris');
+viaje = viajando ('Londres');
+viaje = viajando ('Barcelona');
+
+console.log(viaje);*/
+
+
+//Esta es la misma funcio pero en arrow 
+let viajando =  destino => {
+    return `Viajando a la ciudad de: ${destino}`;
+}
+
+let viaje; 
+viaje = viajando ('Paris');
+viaje = viajando ('Londres');
+viaje = viajando ('Barcelona');
+
+console.log(viaje);
+
+
+
